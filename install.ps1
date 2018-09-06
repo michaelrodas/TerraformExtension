669860abbdb51cfd4c4922dcdb0bc0d14e9e6795
Jonathan Maldonado Rodriguez <jonathan.maldonado.r@gmail.com>
2018-09-06 13:30:11 -0500

    create project

--- install.ps1
+++ install.ps1
@@ -12,11 +12,10 @@ choco install -y visualstudiocode
 choco install -y packer
 choco install -y terraform#> 
 java -jar C:\AutoUpdateToolkit\AutoUpdateToolkit.jar
-Install-Bizagi -componentName ""$componentName"" -channel ""$channel"" -physicalPath ""C:\Bizagi\Enterprise\Projects"" -environmentName ""RNF"" -connectionString "' "$dnsdb" '" -database ""$providerdb""
+Install-Bizagi -componentName $componentName -channel $channel -physicalPath C:\Bizagi\Enterprise\Projects -environmentName RNF -connectionString $dnsdb -database $providerdb
 
 $dnsMachine = "{0}.eastus.cloudapp.azure.com" -f $machineName
 New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $dnsMachine
 $Thumbprint = (Get-ChildItem -Path cert:\localmachine\my  | Where-Object {$_.Subject -Match "CN=$dnsMachine"} ).Thumbprint
 $port= "5986"
 cmd /c "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Port=""$port"" ;Hostname=""$dnsMachine"" ;CertificateThumbprint=""$Thumbprint""}"
-#winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Port="5986" ;Hostname=$dnsMachine ;CertificateThumbprint=$Thumbprint}
