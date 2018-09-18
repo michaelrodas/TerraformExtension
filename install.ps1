041640f6d33d1fb26d726c84a56f02c6376e4290
Jonathan Maldonado Rodriguez <jonathan.maldonado.r@gmail.com>
2018-09-18 15:42:36 -0500

    remove creation of  listener

--- install.ps1
+++ install.ps1
@@ -14,9 +14,3 @@ choco install -y terraform#>
 java -jar C:\AutoUpdateToolkit\AutoUpdateToolkit.jar
 Install-Bizagi -componentName $componentName -channel $channel -physicalPath C:\Bizagi\Enterprise\Projects -environmentName RNF -connectionString $dnsdb -database $providerdb
 Install-Bizagi -componentName SCHEDULER -channel $channel -physicalPath C:\Bizagi\Enterprise\Projects -environmentName RNFScheduler -connectionString $dnsdb -database $providerdb
-
-$dnsMachine = "{0}.eastus.cloudapp.azure.com" -f $machineName
-New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $dnsMachine
-$Thumbprint = (Get-ChildItem -Path cert:\localmachine\my  | Where-Object {$_.Subject -Match "CN=$dnsMachine"} ).Thumbprint
-$port= "5986"
-cmd /c "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Port=""$port"" ;Hostname=""$dnsMachine"" ;CertificateThumbprint=""$Thumbprint""}"
